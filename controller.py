901fd845d3542357f29d2904eb7f3fe77ea1e5fb
Simone Malesardi <simomale98@gmail.com>
2022-09-18 18:53:43 +0200

    lettura dati tiri e conversione in dataFrame. Salvataggio su csv. Operazione di merge iniziata (Team.merge_scores_shots)

--- controller.py
+++ controller.py
@@ -9,7 +9,7 @@ from operation import Operation
 from statistics import Statistic
 from os import walk
 import pandas as pd
-
+import glob, os
 
 class Controller:
     statistics = []
@@ -19,7 +19,8 @@ class Controller:
         self.html = HtmlScrape(season.value)
         self.html.connect()
 
-        self.start() 
+        #self.start() 
+        self.array_teams = []
 
     def start(self):
         self.a_hrefs = self.html.extract_teams() 
@@ -32,26 +33,50 @@ class Controller:
         
         self.array_teams.sort(key=lambda x: x.name) #ordinamento array per nome
 
-    def get_stats(self):
+    def get_stats(self, read): #ottengo statistiche 
+        #il parametro read serve per leggere i csv con le statistiche (True) o fare scraping (False)
         print("Getting stats for each team...")
-        for team in tqdm(self.array_teams):
-            team.get_scores()
+        if read: 
+            self.read_season_csv()
+            #df = pd.read_csv('data.csv')
+        else:
+            for team in tqdm(self.array_teams):
+                team.get_scores()
+                team.get_shots_for_match() 
         
     def save_stats(self):
         stat = Statistic(type = Operation.save_season_stats.value)
 
         print("Saving stats for each team...")
         for team in tqdm(self.array_teams):
-            team.save_csv('/stats/')
+            team.save_csv('/matches/')
 
         stat.stop_time()
         self.statistics.append(stat)
 
     def read_stats(self):
-        filenames = next(walk(self.season.name+"/stats/"), (None, None, []))[2]  # tutti i file trovati nella cartella
-        file = self.season.name+"/stats/"+filenames[0]
-        df = pd.read_csv(file, index_col=0)
+        filenames = next(walk(self.season.name+"/matches/"), (None, None, []))[2]  # tutti i file trovati nella cartella
+        #file = self.season.name+"/stats/"+filenames[0]
+        #df = pd.read_csv(file, index_col=0)
+        for f in filenames:
+            print(f)
+
+    def read_season_csv(self): #legge i file per popolare la lista di squadre
+        folder = self.season.name
+        os.chdir(folder+'/shots/')
+        for file in glob.glob("*.csv"):
+            name = file.replace('.csv', '')
+            team = Team(name=name, scores=None, season=folder)
+            team.shot = pd.read_csv(file)
+            team.match = pd.read_csv('../matches/'+file)
+
+            self.array_teams.append(team)
+
+    def merge_teams(self):
+        for team in self.array_teams:
+            team.merge_scores_shots()
         
+
         
 class Season(Enum):
     Season20_21 = "https://fbref.com/it/comp/11/2020-2021/Statistiche-di-Serie-A-2020-2021"
@@ -59,8 +84,12 @@ class Season(Enum):
     Season22_23 = "https://fbref.com/it/comp/11/Statistiche-di-Serie-A"
 
 controller = Controller(Season.Season21_22)
-controller.extract_team()
-controller.get_stats()
+controller.read_season_csv()
+controller.merge_teams()
+
+#controller.extract_team()
+#controller.get_stats(True)
+
 #controller.save_stats()
 #controller.read_stats()
 
--- team.py
+++ team.py
@@ -10,13 +10,13 @@ class Team:
 
     def get_scores(self): #ottiene i punteggi delle partite giocate
         self.data = requests.get(self.link)
-        match = pd.read_html(self.data.text, match="Punteggi e partite")[0]
-        self.dataset = match[match['Competizione'] == 'Serie A'] #filtro per Serie A, devo salvare solo la serie A, non mi interessa di Champions e altre partite
+        self.match = pd.read_html(self.data.text, match="Punteggi e partite")[0]
+        self.dataset = self.match[self.match['Competizione'] == 'Serie A'] #filtro per Serie A, devo salvare solo la serie A, non mi interessa di Champions e altre partite
         
-    def save_csv(self, folder):
+    def save_csv(self, folder): #salva il dataFrame in csv
         self.dataset.to_csv(self.season.name+folder+self.name+'.csv')
 
-    def get_shots_for_match(self): #ottiene i tiri fatti in ciascuna partita dalla squadra
+    def get_shots_for_match(self): #ottiene i tiri fatti in ciascuna partita giocata dalla squadra
         soup = BeautifulSoup(self.data.text, features="lxml")
         links = soup.find_all('a')
         
@@ -24,10 +24,16 @@ class Team:
         links = [l for l in links if l and 'all_comps/shooting/' in l]
         
         data = requests.get(f"https://fbref.com{links[0]}")
-        shot = pd.read_html(data.text, match="Tiri")[0]
-        #rslt_df = shot[shot['Competizione'] != ' '] 
-        self.dataset = shot
-        self.save_csv('/shots/')
-        #print()
 
+        self.shot = pd.read_html(data.text, match="Tiri")[0]
+
+        self.shot.columns = self.shot.columns.droplevel()
+        self.dataset = self.shot[self.shot['Competizione'] == 'Serie A']
+        #self.save_csv('/shots/')
         
+    def merge_scores_shots(self):
+        #self.dataset = pd.merge(self.match, self.shot, on = id, how='inner')
+        #self.save_csv('/merge_stats_shots/')
+        print("merge")
+        #self.dataset = self.match.merge(self.shot[["Data", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")      
+        #self.save_csv('/merge_stats_shots/')
