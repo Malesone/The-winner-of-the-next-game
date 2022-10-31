d720157ac776fcddd12ab19b08f6643a28ba9dfe
Simone Malesardi <simomale98@gmail.com>
2022-10-31 11:29:17 +0100

    sistemazione analisi, organizzazione in cartelle

--- util_strings.py
+++ util_strings.py
@@ -5,16 +5,16 @@ path_stats = 'Serie A/'
 championship = path_stats+'Championships/{}.csv' #file che contiene tutte le partite del campionato (giocate e non giocate, con il relativo risultato) -> una stagione
 stats = path_stats+'Stats/'
 statistics = stats+'{}.csv' #dataset contenente tutte le statistiche utili per ciascuna squadra in ciascun match (1 season)
-merged_statistics = path_stats+'all stats.csv' #dataset contenente le statistiche per ciascuna squadra in ciascun match giocato (tutte le stagioni)
-matches_merged = path_stats+'matchesMerged/matches.csv' #unisce i match del punto precedente
+merged_statistics = path_stats+'all_stats.csv' #dataset contenente le statistiche per ciascuna squadra in ciascun match giocato (tutte le stagioni)
 matches_description = path_stats+'matches_description.csv' #dataset con testo e predizione di FootballPrediction
 
-dataset_without_text = path_stats+'dataset_without_text.csv' #dataset definitivo per le statistiche
+dataset_without_text = path_stats+'cleaned_stats.csv' #dataset definitivo per le statistiche
 completed_dataset = path_stats+'completed_dataset.csv' #dataset completo, con statistiche, testo e predizione di FootballPrediction
     ###### CSV da lavorare ######
 
     ###### CSV completi ######
-ranking = path_stats+'{}.csv' 
+ranks = path_stats+'Ranks/'
+ranking = ranks+'{}.csv' 
 
 cleaned_matches_description = path_stats+'cleaned_descriptions.csv'
 championship_actual_season = path_stats+'2022-2023 championship.csv'
